68e7c0f5875f11211b80a4a15cc74ce6e2445897
AldoDiaz01 <vitome77@hotmail.com>
2019-08-22 18:36:56 -0500

    add youngphysicist.java

--- youngphysicist.java
+++ youngphysicist.java
@@ -1,1 +1,26 @@
-#Pseudo intento de programa xdxd
+import java.util.Scanner;
+public class youngphysicist
+{
+    public static void main(String[] args)
+    {
+        Scanner teclado = new Scanner(System.in);
+        int n = teclado.nextInt();
+        int a = 0;
+        int b = 0;
+        int c = 0;
+        for(int i=0;i<n;i++)
+        {
+            a += teclado.nextInt();
+            b += teclado.nextInt();
+            c += teclado.nextInt();
+        }
+        if(a==0&&b==0&&c==0)
+        {
+            System.out.println("YES");
+        }
+        else
+        {
+            System.out.println("NO");
+        }
+    }
+}
